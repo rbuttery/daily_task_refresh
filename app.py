7fd5d98665be088cb19f2323d9967cbc1513fa3a
Robert Buttery <robertbuttery1441@gmail.com>
2024-05-16 12:02:59 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from notion_client import NotionClient  
+from google_client import GoogleClient
+
+
+
+@st.cache_data()
+def list_notion_tasks_by_google_task_id():
+        # Query the notion "Items" database
+        notion_items = NotionClient().get_database_rows(os.getenv("NOTION_DATABASE_ID"))['results']
+        
+        def try_task_id(task):
+            try:
+                return task['properties']['Task ID']['rich_text'][0]['text']['content']
+            except:
+                return None
+
+        # This list contains unique task ids from the Notion Items database
+        return list(set([i for i in [try_task_id(x) for x in notion_items] if i != None]))
+    
+
+def push_google_tasks_to_notion():
+    current_notion_ids = list_notion_tasks_by_google_task_id()
+    for task in GoogleClient().list_tasks():
+        
+        # if the google task id is already in notion, skip it
+        if task['id'] in current_notion_ids:
+            # logging.info(f"Task {task['id']} already exists in Notion")
+            continue
+        
+        # if google task does not have a title skip it
+        if task['title'] == '':
+            # logging.info(f"Task {task['id']} does not have a title")
+            continue
+            
+        else:
+            icon = {'type': 'external',
+                    'external': {'url': 'https://www.notion.so/icons/square_green.svg'}}
+            
+            props = {
+                "Item": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": task['title']
+                            }
+                        }
+                    ]
+                },
+                "Done": {
+                    "checkbox": False if task['status'] == "needsAction" else True
+                },
+                "Task ID": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": task['id']
+                            }
+                        }
+                    ]
+                }
+            }
+            
+            try:
+                props['Due'] = {
+                        'type': 'date',
+                        'date': {
+                            'start': task['due'].replace('Z', '') if task['due'] else None,
+                            'end': None,
+                            'time_zone': None
+                        }
+                    }
+            except KeyError:
+                pass
+            
+            db_id = os.getenv("NOTION_DATABASE_ID")
+            new_page = NotionClient().create_page(database_id=db_id, properties=props, icon=icon)
+            # logging.info(f"Created task {task['id']} in Notion")
+        
+st.title("Google Tasks to Notion")
+
+st.button("Push Google Tasks to Notion", on_click=push_google_tasks_to_notion)
---
+++ notion_client.py
@@ -0,0 +1,60 @@
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+import requests
+import json
+
+# setup a log under notion.log
+import logging
+logging.basicConfig(filename="notion.log", level=logging.INFO)
+
+# create a class to interact with the Notion API
+class NotionClient:
+    def __init__(self):
+        # update the log
+        logging.info("Initializing NotionClient")
+        self.token = os.getenv("NOTION_API_KEY")
+        self.url = "https://api.notion.com/v1"
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28"
+        }
+
+    def search(self, query, page_or_database="database"):
+        payload = {
+            "query": query,
+            "filter": {
+                "value": page_or_database,
+                "property": "object"
+            }
+        }
+        response = requests.post(url=f"{self.url}/search", headers=self.headers, json=payload)
+        if 'results' in response.json():
+            return response.json()['results']
+        else:
+            return response.json()
+        
+    def get_database(self, database_id):
+        response = requests.get(url=f"{self.url}/databases/{database_id}", headers=self.headers)
+        return response.json()
+    
+    def get_database_rows(self, database_id):
+        response = requests.post(url=f"{self.url}/databases/{database_id}/query", headers=self.headers)
+        return response.json()
+    
+    def get_page(self, page_id):
+        response = requests.get(url=f"{self.url}/pages/{page_id}", headers=self.headers)
+        return response.json()
+    
+    def create_page(self, database_id, title, properties):
+        payload = {
+            "parent": {
+                "database_id": database_id
+            },
+            "properties": properties
+        }
+        response = requests.post(url=f"{self.url}/pages", headers=self.headers, json=payload)
+        return response.json()
+
